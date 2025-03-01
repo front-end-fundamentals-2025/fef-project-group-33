7ac8c3e505edc39bedcffed16bd43d59b408b481
brr <skibidibada@users.noreply.github.com>
2025-03-01 13:15:47 +0100

    assdsadas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-ul.getElementsByClassName("searchlist").style.display = "none";
+document.getElementById("myUL").style.display = "none";
 function myFunction() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("myInput");
@@ -7,12 +7,20 @@ function myFunction() {
   li = ul.getElementsByClassName("searchlist");
 
   for (i = 0; i < li.length; i++) {
+    document.getElementById("myUL").style.display = "none";
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "block";
+      console.log(filter.length);
     } else {
       li[i].style.display = "none";
+      console.log(filter.length);
+    }
+    if (filter.length > 0) {
+      document.getElementById("myUL").style.display = "block";
+    } else {
+      document.getElementById("myUL").style.display = "none";
     }
   }
 }
