5302965ebba91da6ca541d7a0ca18ac8b587fe0e
brr <skibidibada@users.noreply.github.com>
2025-03-01 15:16:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,11 +12,10 @@ function myFunction() {
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "block";
-      console.log(filter.length);
     } else {
       li[i].style.display = "none";
-      console.log(filter.length);
     }
+
     if (filter.length > 0) {
       document.getElementById("myUL").style.display = "block";
     } else {
