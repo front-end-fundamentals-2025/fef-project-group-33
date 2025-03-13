c3e9d82cf69d7083e44120afaf60b976f6b3f09e
Peri Muhtaroglu <p.muhtaroglu@gmail.com>
2025-03-13 12:55:05 +0100

    hh

--- js/main.js
+++ js/main.js
@@ -209,23 +209,3 @@ themeSwitch.addEventListener("click", () => {
     disableDarkmode();
   }
 });
-
-// LOGIN AND SIGNUP
-const form = document.getElementById("form");
-const firstname_input = document.getElementById("firstname-input");
-const email_input = document.getElementById("email-input");
-const password_input = document.getElementById("password-input");
-const repeat_password_input = document.getElementById("repeat-password-input");
-
-form.addEventListener('submit', (e) => {
-  // e.preventDefault() Prevent Submit
-
-  let errors = []
-
-  if(firstname_input){
-    errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value)
-  } 
-  else{
-    errors =
-  }
-})
